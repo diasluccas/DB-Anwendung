08287dc4a6c990541ae8a26f6bba682ae9bad5a6
Felixx222 <weber.felix-24@stud.dhbw-ravensburg.de>
2026-04-28 12:21:08 +0200

    Add files via upload

--- rennenpflegen.php
+++ rennenpflegen.php
@@ -1,4 +1,22 @@
 <h4>Meine Rennen pflegen</h4>
-<h5>Felix</h5>
-<br></br>
+<?php
+include 'db.php';
+
+if (isset($_POST['rennen_speichern'])) {
+    $datum = $_POST['datum'];
+    $startort = $_POST['startort'];
+    $km = $_POST['km'];
+    $hoehenmeter = $_POST['hoehenmeter'];
+    $steigung = $_POST['steigung'];
+
+    $sql = "INSERT INTO Rennen (Datum, Startort, Kilometer, Hoehenmeter, MaxSteigung) 
+            VALUES ('$datum', '$startort', '$km', '$hoehenmeter', '$steigung')";
+
+    if (mysqli_query($connection, $sql)) {
+        echo "Erfolg!";
+    } else {
+        echo "Fehler: " . mysqli_error($connection);
+    }
+}
+?>
 
