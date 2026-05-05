5bbd8962f0eff391b8ba5cca2828ff57852d707a
Felixx222 <weber.felix-24@stud.dhbw-ravensburg.de>
2026-05-05 08:52:33 +0200

    Add files via upload

--- rennenpflegen.php
+++ rennenpflegen.php
@@ -1,22 +1,51 @@
-<h4>Meine Rennen pflegen</h4>
 <?php
 include 'db.php';
 
-if (isset($_POST['rennen_speichern'])) {
+if (isset($_POST['Rennen_erstellen'])) {
+    // Daten aus dem Formular holen
     $datum = $_POST['datum'];
-    $startort = $_POST['startort'];
+    $ort = $_POST['startort'];
     $km = $_POST['km'];
-    $hoehenmeter = $_POST['hoehenmeter'];
+    $hoehe = $_POST['hoehenmeter'];
     $steigung = $_POST['steigung'];
+    $rvname = $_SESSION['login_rv']; // Name aus der Session
 
-    $sql = "INSERT INTO Rennen (Datum, Startort, Kilometer, Hoehenmeter, MaxSteigung) 
-            VALUES ('$datum', '$startort', '$km', '$hoehenmeter', '$steigung')";
+    // Test: Liegt das Datum in der Vergangenheit?
+    $heute = date("Y-m-d");
 
-    if (mysqli_query($connection, $sql)) {
-        echo "Erfolg!";
+    if ($datum < $heute) {
+        echo "Fehler: Das Datum darf nicht in der Vergangenheit liegen!";
     } else {
-        echo "Fehler: " . mysqli_error($connection);
+        // SQL-Befehl mit Spaltennamen aus Screenshot: Datum, StartOrt, AnzahlKm, HoehenMeter, MaxSteigung, RVName
+        $sql = "INSERT INTO Rennen (Datum, StartOrt, AnzahlKm, HoehenMeter, MaxSteigung, RVName) 
+                VALUES ('$datum', '$ort', '$km', '$hoehe', '$steigung', '$rvname')";
+
+        if (mysqli_query($connection, $sql)) {
+            echo "Rennen erfolgreich gespeichert!";
+        } else {
+            echo "Fehler: " . mysqli_error($connection);
+        }
     }
 }
 ?>
 
+<h4>Neues Rennen anlegen</h4>
+<form method="POST" action="">
+    <label>Datum des Rennens:</label><br>
+    <input type="date" name="datum" required><br>
+
+    <label>Startort:</label><br>
+    <input type="text" name="startort" required><br>
+
+    <label>Anzahl Kilometer:</label><br>
+    <input type="number" name="km" required><br>
+
+    <label>Höhenmeter:</label><br>
+    <input type="number" name="hoehenmeter" required><br>
+
+    <label>Maximale Steigung in Prozent:</label><br>
+    <input type="number" name="steigung" step="0.1" required><br><br>
+
+    <input type="submit" name="Rennen_erstellen" value="Rennen speichern">
+</form>
+<hr />
